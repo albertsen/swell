ba85c7c138a8a56a1bced70ace68a29ca2549893
Juergen Albertsen <mail@juergenalbertsen.de>
2020-02-28 09:58:37 +0100

    Fix tests

--- test/swell/worflow_endpoint_test.exs
+++ test/swell/worflow_endpoint_test.exs
@@ -4,19 +4,21 @@ defmodule Swell.Test.WorkflowServiceTest do
   alias Swell.Rest.Client
   @url "http://localhost:8080/workflowdefs"
 
-  test "CRUD" do
+  test "Worfklow definition CRUD" do
     json = File.read!("test/data/workflowdefs/fulfilorder.json")
     workflow_def = Jason.decode!(json, keys: :atoms)
-    {status, _} = Client.post(@url, workflow_def)
+    {status, doc} = Client.post(@url, workflow_def)
     assert status == :created
+    assert doc == workflow_def
     {status, _} = Client.post(@url, workflow_def)
     assert status == :conflict
     {status, workflow_def_created} = Client.get("#{@url}/#{workflow_def.id}")
     assert status == :ok
     assert workflow_def == workflow_def_created
     workflow_def = %{workflow_def | description: "Updated description"}
-    {status, _} = Client.put("#{@url}/#{workflow_def.id}", workflow_def)
+    {status, doc} = Client.put("#{@url}/#{workflow_def.id}", workflow_def)
     assert status == :ok
+    assert doc == workflow_def
     {status, worflow_def_updated} = Client.get("#{@url}/#{workflow_def.id}")
     assert status == :ok
     assert workflow_def == worflow_def_updated
@@ -28,7 +30,7 @@ defmodule Swell.Test.WorkflowServiceTest do
     assert status == :not_found
   end
 
-  test "schema validation" do
+  test "Workflow definition Schema Validation" do
     {status, res} = Client.post(@url, %{invalid: "document"})
     assert status == :unprocessable_entity
     assert res == %{errors: [%{path: "#", reason: "Required properties id, actionHandlers, steps were not present."}]}
